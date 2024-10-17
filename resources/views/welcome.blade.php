a49c6b75e6ccdd5d25398a97909b58fa6f3483ad
Eduard-Condurache <eduardcondurache96@gmail.com>
2024-10-17 18:49:22 +0200

    Completato esercizio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,12 +2,33 @@
 
 @section('main-content')
 
-<div>
+<main>
     
-</div>
-
-<div>
-    <img src="{{ Vite::asset('resources/img/testimg.webp') }}" alt="">
-</div>
-
+    <div class="container">
+        <div class="row">
+            @foreach($movies as $movie)
+            <div class="card col-3">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        Titolo: {{ $movie['title'] }}
+                    </h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">
+                        Titolo originale: {{ $movie['original_title'] }}
+                    </h6>
+                    <p class="card-text">
+                        Nationalità: {{ $movie['nationality'] }}
+                    </p>
+                    <p class="card-text">
+                        Data: {{ $movie['date'] }}
+                    </p>
+                    <p class="card-text">
+                        Voto: {{ $movie['vote'] }}
+                    </p>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+    
+</main>
 @endsection
